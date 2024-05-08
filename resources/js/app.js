de0ef249669cf4e1b7ec0f6a5c46b415b97a4bed
Bacio001 <bacio0011@gmail.com>
2024-05-08 11:05:09 +0200

    chore: Add Tailwind CSS and Vue.js dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue'
+import counter from '../views/components/counter.vue'
+
+const app = createApp()
+
+app.component('counter', counter)
+
+app.mount('#app')
